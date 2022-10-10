c1a9967d60414d5df2ecce124f762bd4941cf20e
audreyamar <71273052+audreyamar@users.noreply.github.com>
2022-10-10 17:55:27 +0200

    FEAT: Android app (get .json fonctional) Two page on the app with some boutons

---
+++ MyArea/app/src/main/java/com/example/myarea/FirstFragment.kt
@@ -0,0 +1,94 @@
+package com.example.myarea
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.example.myarea.databinding.FragmentFirstBinding
+import kotlinx.serialization.json.*
+
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class FirstFragment : Fragment() {
+    private var lastResponse : String = "";
+
+    private var _binding: FragmentFirstBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    fun getRequest(){
+        // Instantiate the RequestQueue.
+        val queue = Volley.newRequestQueue(MainActivity.instance)
+        val url = "http://10.0.2.2:8080/about.json"
+
+// Request a string response from the provided URL.
+        val stringRequest = StringRequest(
+            Request.Method.GET, url,
+            Response.Listener<String> { response ->
+                //
+                println("GET Request : ${response}")
+                lastResponse = response;
+                MyParse(lastResponse);
+            },
+            Response.ErrorListener { println( "That didn't work!" )})
+
+// Add the request to the RequestQueue.
+        queue.add(stringRequest)
+    }
+
+    fun MyParse(string: String) {
+        var response = Json.parseToJsonElement(string);
+        var server = response.jsonObject.get("server");
+        var time = server?.jsonObject?.get("current_time");
+        if (time != null) {
+            println("ICI LE TEMPS : " + time.jsonPrimitive.int)
+        }else {
+            println("AHZIUEOV DAVRUYOEZV OUYV")
+        }
+        var services = server?.jsonObject?.get("services")?.jsonArray;
+        var serList : List<JsonObject> = listOf();
+        var serviceList = serList.toMutableList()
+        for (i in 0 until services?.size!!) {
+            var service = services.get(i).jsonObject;
+            serviceList.add(service);
+            println(service.get("name")?.jsonPrimitive.toString())
+        }
+        for (i in 0 until serviceList.size) {
+            println("ma liste :" + serviceList[i])
+        }
+    }
+
+    override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.buttonFirst.setOnClickListener {
+            //findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+            Log.d("TAG", "tbleubleublue ca me parait etre un bon quoi feur")
+            getRequest()
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
