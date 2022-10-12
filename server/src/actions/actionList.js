21b8ce9b478f94c1f39d8e81eb003aa152e2136c
EvanMevel <evan.mevel@epitech.eu>
2022-10-12 15:32:11 +0200

    FIX removing useless action

--- server/src/actions/actionList.js
+++ server/src/actions/actionList.js
@@ -1,7 +1,6 @@
 
 const ARList = require("../ARList");
 const Action = require("./action");
-const FB_L_POST = require("./fb_listen_post");
 const SPOTIFY_LIKE = require("./spotify_action_like");
 const GET_WEATHER = require("./get_weather_action");
 
@@ -9,7 +8,6 @@ class ActionList extends ARList {
 
     constructor() {
         super("actions", Action);
-        this.add(FB_L_POST, "facebook_action_post", "Facebook Listen Post", "Listen to a facebook post", "facebook");
         this.add(SPOTIFY_LIKE, "spotify_action_like", "Spotify Listen Likes", "Listen to user liking a track on spotify", "spotify");
         this.add(GET_WEATHER, "get_weather_action", "Weather", "Gets Weather", "meteo_concept");
     }
--- server/src/reactions/reactionList.js
+++ server/src/reactions/reactionList.js
@@ -1,7 +1,6 @@
 
 const ARList = require("../ARList");
 const Reaction = require("./reaction");
-const FB_POST = require("./fb_post");
 const DEEZER_LIKE = require('./deezer_reaction_like');
 const DISCORD_WEATHER = require('./discord_reaction_weather');
 
@@ -9,7 +8,6 @@ class ReactionList extends ARList {
 
     constructor() {
         super("reactions", Reaction);
-        this.add(FB_POST, "facebook_reaction_post", "Facebook Post", "Post a facebook post", "facebook");
         this.add(DEEZER_LIKE, "deezer_reaction_like", "Deezer Like", "Likes the track on Deezer", "deezer");
         this.add(DISCORD_WEATHER, "discord_reaction_weather", "Discord Weather", "Sends message on discord with weather", "discord");
     }
