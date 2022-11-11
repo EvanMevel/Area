97b570ff40512b5c5017cd262264fc194f0e6c3b
Théo GUGUEN <72015839+theoguguen@users.noreply.github.com>
2022-11-11 17:33:00 +0100

    FIX Services for youtube, now one for oauth and one for no auth

--- server/src/services.js
+++ server/src/services.js
@@ -7,8 +7,8 @@ class Services {
         this.add("deezer", "Deezer", 1);
         this.add("discord", "Discord", 0);
         this.add("meteo_concept", "Météo Concept", 0);
-        this.add("youtube_oauth", "Youtube OAuth", 1);
-        this.add("youtube", "Youtube", 0);
+        this.add("youtube_noauth", "Youtube NoAuth", 0);
+        this.add("youtube", "Youtube", 1);
     }
 
     add(name, displayName, oauth) {
