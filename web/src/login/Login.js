7e3adc3e46360e8ea31ba5a71fbd1e12f08a8b56
unknown <eleonore.wichegrod@epitech.eu>
2022-11-12 06:49:15 +0100

    CREATE: beautiful front login

--- web/src/login/Login.js
+++ web/src/login/Login.js
@@ -1,25 +1,24 @@
-import React, {useEffect, useState} from "react";
+import React, {useState} from "react";
 import {login} from "../api";
 import AuthLogins from "./AuthLogins";
-import {useNavigate, useSearchParams} from "react-router-dom";
+import {useNavigate} from "react-router-dom";
 import {AxiosError} from "axios";
-import {Store} from "react-notifications-component";
 
 async function loginUser(credentials) {
-    const {data, error} = await login(credentials);
-    if (error) {
-        if (error instanceof AxiosError && error.response != null) {
-            return {error: error.response.data};
-        }
-        return {error: error};
-    }
+    try {
+        const {data} = await login(credentials);
 
-    return {token: data.token};
+        return {token: data.token};
+    } catch (err) {
+        if (err instanceof AxiosError) {
+            return {error: err.response.data};
+        }
+        return {error: err};
+    }
 }
 
 export default function Login() {
     let navigate = useNavigate();
-    const [searchParams] = useSearchParams();
     const [err, setErr] = useState();
     const [enabled, setEnabled] = useState(true);
 
@@ -43,35 +42,62 @@ export default function Login() {
         }
     }
 
-    useEffect(() => {
-        const noUser = searchParams.get("noUser");
-        if (noUser) {
-            Store.addNotification({
-                message: "No area account linked with this " + noUser + " account!",
-                type: "danger",
-                insert: "top",
-                container: "top-center",
-                dismiss: {
-                    duration: 5000
-                }
-            });
-        }
-    }, [])
+    const containere = {
+        "display": "flex",
+        "align-items": "center",
+        "flex-direction": "column",
+        "justify-content": "space-between"
+    }
+
+    const img = {
+        "margin-left": "auto",
+        "margin-right": "auto",
+        "display": "flex",
+        "align-items": "center",
+        "flex-direction": "column",
+        "width": "15%"
+    }
+
+    const button = {
+        "background-color": "#5D3FD3",
+        "border": "none",
+        "color": "white",
+        "padding": "10px 64px",
+        "text-align": "center",
+        "text-decoration": "none",
+        "display": "inline-block",
+        "font-size": "16px",
+        "margin": "4px 2px",
+        "cursor": "pointer"
+    }
+
+    const input = {
+        "width": "100%",
+        "padding": "10px 20px",
+        "margin": "8px 0",
+        "box-sizing": "border-box",
+        "border": "none",
+        "border-bottom": "1px solid"
+    }
+
+    var logo = require ('./img/logo.png')
 
     return <div>
-        <form onSubmit={handleSubmit} onChange={() => {setErr(null)}}>
+
+        <img src={logo} style={img}></img>
+        <form onSubmit={handleSubmit} onChange={() => {setErr(null)}} style={containere}>
             <div>
-                <input type={"username"} name={"name"} placeholder="Name or email"/>
+                <input type={"username"} name={"name"} placeholder="Name or email" style={input}/>
             </div>
             <div>
-                <input type={"password"} name={"password"} placeholder="Password"/>
+                <input type={"password"} name={"password"} placeholder="Password" style={input}/>
             </div>
             <div>
-                <button type="submit" disabled={!enabled}>Login</button>
+                <button type="submit" disabled={!enabled} style={button}>Login</button>
             </div>
         </form>
 
-        <a href="/register">Dont have an account? Register here</a>
+        <a href="/register" style={containere}>Dont have an account? Register here</a>
 
         {err && <h2>{err.message}</h2>}
 
